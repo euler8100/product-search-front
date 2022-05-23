1fade323bc73d2425972f060a546e0409d0a2733
Euler <dougbeeuler@gmail.com>
2022-05-23 07:03:36 +0000

    fix error

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,8 @@ module.exports = defineConfig({
     allowedHosts: "all",
     // public: "https://0945-197-234-221-168.ngrok.io",
     // webSocketServer: require("child_process").execSync("gp url 8080").toString().trim(),
-    client: {
-      webSocketURL:  require("child_process").execSync("gp url 8080").toString().trim(),
-    },
+    // client: {
+    //   webSocketURL:  require("child_process").execSync("gp url 8080").toString().trim(),
+    // },
   },
 });
